067dfff8f20e9f2cfd7c1d71129ac5e4fb16447e
vigor-13 <vigor13.dev@gmail.com>
2024-05-22 21:58:56 +0900

    feat: add opacity control props to overlay component

--- src/ui/core/Overlay/Overlay.tsx
+++ src/ui/core/Overlay/Overlay.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import { StyleSheet, ViewProps, View } from 'react-native';
 
-export interface OverlayProps extends ViewProps {}
+export interface OverlayProps extends ViewProps {
+  opacity?: number;
+}
 
 export const Overlay: React.FC<OverlayProps> = (props) => {
-  const { children, style } = props;
+  const { children, opacity = 0.6, style } = props;
 
-  return <View style={[styles.container, style]}>{children}</View>;
+  return (
+    <View
+      style={[
+        styles.container,
+        {
+          backgroundColor: `rgba(0,0,0, ${opacity ? opacity : 0.6})`,
+        },
+        style,
+      ]}
+    >
+      {children}
+    </View>
+  );
 };
 
 export const styles = StyleSheet.create({
@@ -16,7 +30,6 @@ export const styles = StyleSheet.create({
     left: 0,
     right: 0,
     bottom: 0,
-    backgroundColor: 'rgba(0,0,0, 0.6)',
     flex: 1,
   },
 });
