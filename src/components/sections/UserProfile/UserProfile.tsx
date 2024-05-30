12f283ffa5cf64833379d43ee0601a98eea8f565
vigor-13 <vigor13.dev@gmail.com>
2024-05-30 19:39:12 +0900

    feat: implement UserProfile component

---
+++ src/components/sections/UserProfile/UserProfile.tsx
@@ -0,0 +1,36 @@
+import { useAuthStore } from '@/stores';
+import { Avatar, Text, tokens } from '@/ui';
+import React from 'react';
+import RN from 'react-native';
+
+export const UserProfile: React.FC = () => {
+  const { session } = useAuthStore();
+  const user = session?.user;
+
+  return (
+    user && (
+      <RN.View style={styles.container}>
+        <Avatar />
+        <RN.View style={styles.textContainer}>
+          <Text style={styles.userNameText}>{user.user_metadata.name}</Text>
+        </RN.View>
+      </RN.View>
+    )
+  );
+};
+
+const styles = RN.StyleSheet.create({
+  container: {
+    justifyContent: 'flex-end',
+    padding: tokens.st.space[200],
+    minHeight: 160,
+  },
+  textContainer: {
+    marginTop: tokens.st.space[250],
+  },
+  userNameText: {
+    fontSize: tokens.st.font.size.xl2,
+    fontWeight: tokens.st.font.weight.extrabold as any,
+    lineHeight: tokens.st.font.size.xl2,
+  },
+});
